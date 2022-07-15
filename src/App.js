dbc118325c1b46ce1e650fe06a902875514798d9
Yura Sabat <yrasabat@gmail.com>
2022-07-15 14:47:30 +0300

    memo_useMemo_useCallback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {memo, useCallback, useMemo, useState} from 'react';
+
+
+const Test = memo(({cartSum, onClick}) => {
+    console.log("Test");
+
+
+    return (
+        <h3 onClick={() => onClick(Math.random())}>
+            Test shows cartSum: {cartSum.val}
+        </h3>
+    );
+}, );
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [counter, setCounter] = useState(0);
+
+    // const arr = [21, 56, 77, 93, 1, 7, 92, 11];
+
+    const [arr, setArr] = useState([21, 56, 77, 93, 1, 7, 92, 11]);
+
+
+    const cartSum = useMemo(
+        () => ({val: arr.reduce((acc, el) => (acc + el), 0)}),
+        []
+    );
+
+    // const cartSum = ({val: arr.reduce((acc, el) => (acc + el), 0)});
+
+    const onClickTest = useCallback((num) => {
+        console.log(num)
+    }, [arr])
+
+    return (
+        <div>
+            {/*<h3 onClick={() => setCounter((prev) => prev+1)}>*/}
+            <h3 onClick={() => setArr((prev) => [...prev, Math.random()])}>
+                Counter: {counter}
+            </h3>
+            <Test cartSum={cartSum} onClick={onClickTest}/>
+        </div>
+    );
 }
 
 export default App;
